bcae255e8b04a829e7c929e593052e4a4c5ac277
Alberto Soriano <albertosoriano.e@gmail.com>
2022-09-30 18:04:44 -0500

    Agregado Descripcion del problema

--- CSAcademy/Swap_Permutation_v1/Swap_Permutation_v1.py
+++ CSAcademy/Swap_Permutation_v1/Swap_Permutation_v1.py
@@ -1,7 +1,12 @@
 # N : Largo del vector de permutacion
-# La lista esta en orden ascendente
+# La lista esta en orden ascendente, de 1 hasta N.
 # M : Largo del vector de intercambios
-# Remover un par de la secuencia de M, es una lista de tuplas de 2,
-# que representan indices de la lista N
-# hay que remover una tupla de M, 
+# M es una lista de tuplas de 2 con los indices a intercambiar
+# K : indice donde se debe encontrar el valor de 1
+
+# Objetivo: Remover una de las tuplas de la lista de M, y realizar los cambios de M en orden,
+# donde el indice K sea el valor 1
+# Output: Imprimir el indice contando desde 1 de la tupla que se removio
+
+
 
