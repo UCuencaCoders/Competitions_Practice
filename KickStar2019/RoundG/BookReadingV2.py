39aa69c37e0703a2dcc3b9a79f2070262b938584
Alberto Soriano <albertosoriano.e@gmail.com>
2022-09-29 20:52:35 -0500

    Codigo v2 terminado con un bucle

--- KickStar2019/RoundG/BookReadingV2.py
+++ KickStar2019/RoundG/BookReadingV2.py
@@ -11,3 +11,12 @@ for case in range(test_cases):
     
     total_pages_readed = 0 # Inicializamos el numero de paginas leidas
     
+    for reader in readers_iter_list:
+        total_pages = book_pages // reader # Obtenemos el numero de paginas que va a leer el lector sin contar las malas
+        bad_pages_not_readed = 0 # Inicializamos el numero de paginas malas que no va a leer el lector
+        bad_pages_not_readed += len([x for x in bad_pages_list if x % reader == 0]) # Obtenemos el numero de paginas malas que no va a leer el lector
+        total_pages -= bad_pages_not_readed # Restamos el numero de paginas malas que no va a leer el lector al total de paginas que va a leer el lector
+        
+        total_pages_readed += total_pages # Sumamos las paginas leidas por el lector al total de paginas leidas
+    
+    print("Case #{}: {}".format(case+1, total_pages_readed)) # Imprimimos el resultado por caso de prueba
