a76cf0ec1d96745002e03cd6762ae7eab40ad63b
Alberto Soriano <albertosoriano.e@gmail.com>
2022-09-30 19:37:28 -0500

    Creada estructura inicial, recibe input y creamos funcion de swaping

--- CSAcademy/Swap_Permutation_v1/Swap_Permutation_v1.py
+++ CSAcademy/Swap_Permutation_v1/Swap_Permutation_v1.py
@@ -8,5 +8,30 @@
 # donde el indice K sea el valor 1
 # Output: Imprimir el indice contando desde 1 de la tupla que se removio
 
+#------------------------------------------------------------
+#Creamos una funcion para intercambiar el valor de los valores en N
+def swap(N_list, index_1, index_2):
+    N_list[index_1], N_list[index_2] = N_list[index_2], N_list[index_1]
+
+#------------------------------------------------------------
+# --- MAIN ---
+
+#Obtenemos los datos
+N, M, K = map(int, input().split())
+M_list = []
+
+for i in range(M):
+    M_list.append(tuple(map(int, input().split())))
+
+#Creamos la lista N Creciente
+N_list = list(range(1, N+1))
+
+#Realizamos los cambios para encontrar el valor 1 en el indice K
+
+discard = 1  #Variable para almacenar el indice de la tupla que se removio
+
+for i in range(M):
+    pass
+
 
 
