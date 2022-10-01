5f382b8a49f039a244c31b691c3690992aee378a
Alberto Soriano <albertosoriano.e@gmail.com>
2022-09-30 23:19:23 -0500

    Avance de Swap_Permutatio V2, falta cambios y guardar datos

--- CSAcademy/Swap_Permutation_v1/Swap_Permutation_v2.py
+++ CSAcademy/Swap_Permutation_v1/Swap_Permutation_v2.py
@@ -9,15 +9,35 @@
 # --- MAIN ---
 
 #Obtenemos los datos de entrada
-from CSAcademy.Swap_Permutation_v1.Swap_Permutation_v1 import M_list
-
 
 N, M, K = map(int, input().split())
-M_list_1 = []
-M_list_2 = []
+M_list = []
 
 for i in range(M):
     input_1, input_2 = map(int, input().split())
-    M_list_1.append(input_1)
-    M_list_2.append(input_2)
+    M_list.append(input_1)
+    M_list.append(input_2)
 
+#Guardamos la posicion de 1 en N_list
+pos_original = 1
+pos_restore = 1
+
+#Realizamos los cambios para encontrar el valor 1 en el indice K solo en el valor de 1
+for discard in range(M):
+    
+    used_indices = {} # Diccionario que guarda los indices usados en cada iteracion
+    
+    #bucle de cambio de posicion de 1 en N_list, sin saber la cantidad de cambios
+    while True:
+        if discard != 0:
+            pass
+        
+        prox_pos = M_list.index(pos_original) # Buscamos la posicion de 1 en N_list
+        
+        if prox_pos % 2 != 0: prox_pos -= 1 # Si la posicion es impar, se resta 1 para obtener la posicion de la tupla
+        prox_pos /= 2
+        prox_pos += 1
+        # asi obtenemos la posicion de la tupla en M_list
+        
+        if prox_pos != discard:
+            pass
