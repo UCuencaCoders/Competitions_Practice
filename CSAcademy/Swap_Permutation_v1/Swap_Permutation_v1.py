47d66ad36dc597b7fe27c9faf24f0e0d0fe5d7a2
Alberto Soriano <albertosoriano.e@gmail.com>
2022-09-30 20:39:32 -0500

    Terminado el codigo con 30/100 puntos, sobrepasa tiempo de exe en 80ms

--- CSAcademy/Swap_Permutation_v1/Swap_Permutation_v1.py
+++ CSAcademy/Swap_Permutation_v1/Swap_Permutation_v1.py
@@ -9,14 +9,12 @@
 # Output: Imprimir el indice contando desde 1 de la tupla que se removio
 
 #------------------------------------------------------------
-#Creamos una funcion para intercambiar el valor de los valores en N
-def swap(N_list, index_1, index_2):
-    N_list[index_1], N_list[index_2] = N_list[index_2], N_list[index_1]
+# Funciones
 
 #------------------------------------------------------------
 # --- MAIN ---
 
-#Obtenemos los datos
+#Obtenemos los datos de entrada
 N, M, K = map(int, input().split())
 M_list = []
 
@@ -27,11 +25,23 @@ for i in range(M):
 N_list = list(range(1, N+1))
 
 #Realizamos los cambios para encontrar el valor 1 en el indice K
+restore_list = [] # Lista que guarda el estado de N_list en la posicion de descarte
 
-discard = 1  #Variable para almacenar el indice de la tupla que se removio
+for discard in range(M): # Bucle de busqueda, se descarta una tupla por cada iteracion
+    
+    for pos in range(discard-1, M): # Bucle de intercambio de valores en N_list sin contar la tupla descartada
+        if pos == discard:
+            restore_list = N_list.copy() # Guardamos el estado de N_list en la posicion de descarte
+        elif pos >= 0:
+            index_1 = M_list[pos][0] - 1
+            index_2 = M_list[pos][1] - 1
+            N_list[index_1], N_list[index_2] = N_list[index_2], N_list[index_1]
+    
+    if N_list[K-1] == 1: 
+        print(discard+1) # Si el valor 1 esta en el indice K, se imprime el indice de la tupla descartada
+        break # Se termina el bucle de busqueda
+    else: 
+        N_list = restore_list.copy() # Si no, se restaura el estado de N_list y se continua con la siguiente tupla
 
-for i in range(M):
-    pass
 
-
-
+# Puntaje obtenido 30/100, desde el test case 4 para arriba me pasa el tiempo de ejecucion por 80ms.
